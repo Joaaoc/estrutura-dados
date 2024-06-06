fdcab40993fd33045138e64755c5708791c1dfda
Maatthz <129008743+Maatthz@users.noreply.github.com>
2024-06-05 23:54:47 -0300

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -4,21 +4,23 @@
 typedef struct Teste_
 {
 	int value;
-	struct Teste_* node;
+	struct Teste_ *node;
 } Teste;
 
-Teste* criarLista(Teste* node, int value)
+Teste *criarLista(Teste *node, int value)
 {
-	Teste* teste = (Teste*)malloc(sizeof(Teste));
-	if (teste == NULL) { return NULL; }
+	Teste *teste = (Teste *)malloc(sizeof(Teste));
+	if (teste == NULL)
+	{
+		return NULL;
+	}
 	teste->value = value;
 	teste->node = node;
 	return teste;
 }
+// Aloca espaço na memória do tamanho de "Teste" e declarando o valor solicitado pelo usuário ao nó inicial 
 
-//Aloca espaço na memória do tamanho de "Teste"
-
-void mostra(Teste* node)
+void mostra(Teste *node)
 {
 	printf("Mostrando a lista: ");
 	if (node == NULL)
@@ -35,8 +37,7 @@ void mostra(Teste* node)
 }
 // Se o primeiro nó apontar para um valor nulo, retorna que a lista está vazia. Caso contrário, percorre toda lista (enquanto imprime na tela do usuário, até encontrar um nó que aponte para o valor nulo, o que indicará o fim da lista.
 
-
-void mostraEspecifico(Teste* node, int index)
+void mostraEspecifico(Teste *node, int index)
 {
 	printf("Mostrando elemento especifico: ");
 	if (node == NULL)
@@ -62,30 +63,30 @@ void mostraEspecifico(Teste* node, int index)
 	}
 	printf("%d\n", node->value);
 }
-//Se o primeiro nó tiver um valor NULL, indicará que a lista está vazia. Se o index (posicao escolhida pelo usuario) for menor que 0, retornará que é uma posição inválida. Caso contrário, entrará na estrutura de repetição "while" para procurar a posição solicitada, se no meio da busca da posição solicitada encontrar um nó apontando para um nó com valor nulo, irá informar ao usuário que a posição escolhida é maior que a lista criada.
+// Se o primeiro nó tiver um valor NULL, indicará que a lista está vazia. Se o index (posicao escolhida pelo usuario) for menor que 0, retornará que é uma posição inválida. Caso contrário, entrará na estrutura de repetição "while" para procurar a posição solicitada, se no meio da busca da posição solicitada encontrar um nó apontando para um nó com valor nulo, irá informar ao usuário que a posição escolhida é maior que a lista criada.
 
-Teste* inserirNoInicio(Teste* startNode, int value)
+Teste *inserirNoInicio(Teste *startNode, int value)
 {
 	if (startNode == NULL)
 	{
-		printf("Lista vazia.\n");
-		return NULL;
+		Teste *newNode = criarLista(startNode, value);
+		return newNode;
 	}
-	Teste* newNode = criarLista(startNode, value);
+	Teste *newNode = criarLista(startNode, value);
 	printf("%d inserido no inicio da lista.\n", value);
 	return newNode;
 }
-// Caso o primeiro nó seja nulo, informará ao usuário que a lista está vazia. Caso contrário, insere o valor "value" escolhido pelo usuário no início da lista. 
+// Caso o primeiro nó seja nulo, informará ao usuário que a lista está vazia. Caso contrário, insere o valor "value" escolhido pelo usuário no início da lista.
 
-Teste* inserirNoFinal(Teste* startNode, int value)
+Teste *inserirNoFinal(Teste *startNode, int value)
 {
 	if (startNode == NULL)
 	{
-		printf("Lista vazia.\n");
-		return startNode;
+		Teste *newNode = criarLista(startNode, value);
+		return newNode;
 	}
-	Teste* newNode = criarLista(NULL, value);
-	Teste* node = startNode;
+	Teste *newNode = criarLista(NULL, value);
+	Teste *node = startNode;
 	while (node->node != NULL)
 	{
 		node = node->node;
@@ -97,7 +98,7 @@ Teste* inserirNoFinal(Teste* startNode, int value)
 
 // Caso o primeiro nó seja nulo, informará ao usuário que a lista está vazia. Caso contrário, percorre toda a lista até que encontre um nó que aponte para um valor NULO (o que indicará o fim da lista e, consequentemente, o ultimo nó) e insere o nó solicitado pelo usuário após o nó encontrado.
 
-Teste* inserirNoMeio(Teste* startNode, int value, int index)
+Teste *inserirNoMeio(Teste *startNode, int value, int index)
 {
 	if (index < 0)
 	{
@@ -110,7 +111,7 @@ Teste* inserirNoMeio(Teste* startNode, int value, int index)
 	}
 	index -= 1;
 
-	Teste* node = startNode;
+	Teste *node = startNode;
 	int idx = 0;
 	while (idx < index)
 	{
@@ -122,46 +123,52 @@ Teste* inserirNoMeio(Teste* startNode, int value, int index)
 		idx++;
 		node = node->node;
 	}
-	Teste* nextNode = criarLista(node->node, value);
+	Teste *nextNode = criarLista(node->node, value);
 	node->node = nextNode;
 	printf("%d inserido na posicao %d da lista.\n", node->node->value, index + 1);
 	return startNode;
 }
 // Caso o index (posicao na lista) informado pelo usuário seja menor do que zero, retornará que é um valor inválido. Caso seja igual a zero, irá chamar a função inserirNoInicio, já que corresponderá à primeira posição. Caso contrário, percorre a lista até encontrar o nó da posição anterior ao solicitado pelo usuário , depois disso cria o nó com o valor inserido pelo usuário na posição solicitada (Ex: Usuário solicitou criação de um nó com valor 100 na posição 5. O programa irá procurar o nó na posição 4 e criará o nó solicitado após o nó na posição 4).
-Teste* excluirNoInicio(Teste* startNode)
+
+Teste *excluirNoInicio(Teste *startNode)
 {
-	Teste* node = startNode;
+	Teste *node = startNode;
 	if (startNode == NULL)
 	{
 		printf("Lista vazia.\n");
 		return NULL;
 	}
 	printf("%d excluido no inicio da lista.\n", startNode->value);
-	Teste* newNode = node->node;
+	Teste *newNode = node->node;
 	free(startNode);
+	startNode = NULL;
 	return newNode;
 }
 // Caso o primeiro nó seja nulo, informará ao usuário que a lista está vazia. Caso contrário, remove o primeiro nó encontrado.
-Teste* excluirNoFinal(Teste* startNode)
+Teste *excluirNoFinal(Teste *startNode)
 {
 	if (startNode == NULL)
 	{
 		printf("Lista vazia.\n");
 		return startNode;
 	}
-	Teste* node = startNode;
+	if (startNode->node == NULL)
+		return excluirNoInicio(startNode);
+
+	Teste *node = startNode;
 	while (node->node != NULL && node->node->node != NULL)
 	{
 		node = node->node;
 	}
 	printf("%d excluido no final da lista.\n", node->node->value);
-	free(node->node);
+	if (node->node != NULL)
+		free(node->node);
 	node->node = NULL;
 	return startNode;
 }
 // Caso o primeiro nó seja nulo, informará ao usuário que a lista está vazia. Caso contrário, percorre toda a lista até que encontre um nó que aponte para um valor NULO (o que indicará o fim da lista e, consequentemente, o ultimo nó) e exclui o nó encontrado.
 
-Teste* excluirNoMeio(Teste* startNode, int index)
+Teste *excluirNoMeio(Teste *startNode, int index)
 {
 	if (startNode == NULL)
 	{
@@ -178,8 +185,8 @@ Teste* excluirNoMeio(Teste* startNode, int index)
 		return excluirNoInicio(startNode);
 	}
 	index -= 1;
-	Teste* nextNode = NULL;
-	Teste* node = startNode;
+	Teste *nextNode = NULL;
+	Teste *node = startNode;
 	int idx = 0;
 	while (idx < index)
 	{
@@ -201,20 +208,21 @@ Teste* excluirNoMeio(Teste* startNode, int index)
 	return startNode;
 }
 // Caso o index (posicao na lista) informado pelo usuário seja menor do que zero, retornará que é um valor inválido. Caso seja igual a zero, irá chamar a função excluirNoInicio, já que corresponderá à primeira posição. Caso contrário, percorre a lista até encontrar o nó da posição anterior ao solicitado pelo usuário, depois disso cria o nó com o valor inserido pelo usuário na posição solicitada (Ex: Usuário solicitou exclusão de um nó com valor 100 na posição 5. O programa irá procurar o nó na posição 4 e deletará o nó posterior.)
-Teste* esvaziar(Teste* startNode)
+Teste *esvaziar(Teste *startNode)
 {
-	Teste* node = startNode;
+	Teste *node = startNode;
 	while (startNode->node != NULL)
 	{
 		node = excluirNoFinal(startNode);
 	}
+	printf("%d excluido no final da lista.\n", startNode->value);
 	free(node);
 	return NULL;
 }
-//Enquanto o nó estiver apontando para um outro nó com valor válido, este nó será excluído. Ao encontrar o ultimo nó, que estará apontando para um valor NULL, sairá da estrutura de repetição e será excluído com free(node);
+// Enquanto o nó estiver apontando para um outro nó com valor válido, este nó será excluído. Ao encontrar o ultimo nó, que estará apontando para um valor NULL, sairá da estrutura de repetição e será excluído com free(node);
 int main()
 {
-	Teste* lista = criarLista(NULL, 0); // 0
+	Teste *lista = NULL; // 0
 
 	int value, index;
 	int command;
@@ -276,16 +284,16 @@ int main()
 		}
 		else if (command == 6)
 		{
-			excluirNoInicio(lista, value);
+			lista = excluirNoInicio(lista);
 		}
 		else if (command == 7)
 		{
-			lista = excluirNoFinal(lista, value);
+			lista = excluirNoFinal(lista);
 		}
 		else if (command == 8)
 		{
 			printf("Digite a posicao: ");
-			scanf_s("%d %d", &value, &index);
+			scanf_s("%d", &index);
 			printf("\n");
 			lista = excluirNoMeio(lista, index);
 		}
